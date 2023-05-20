e45ee798f74f9f905d1ec3ed8e248ae6ec9a8480
sophiaUfg <131269429+sophiaUfg@users.noreply.github.com>
2023-05-20 13:18:49 -0300

    Remoção de funções que não serão usadas

--- projetoSistemaVendas/avl.h
+++ projetoSistemaVendas/avl.h
@@ -11,10 +11,8 @@ typedef struct node {
 
 Node* insert(Node* raiz, Produto produto);
 Node* remover(Node* raiz, int codigo);
-void atualizarInformacoes(Node* raiz, int codigo, char novoNome[100], float novoPreco, int novaQuantidade);
 Node* buscar(Node* raiz, int codigo);
 void listarProdutos(Node* raiz);
-void gerenciarEstoque(Node* raiz, int codigo, int quantidade);
 void liberaArvore(Node* raiz);
 void altura(Node* no);
 
